49a34f76b703e16ef90aba0eaac9da77466150a8
Johnny Vardel <80030559+lemonadebusinesssolutions@users.noreply.github.com>
2025-04-07 14:03:52 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import queryClient from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
